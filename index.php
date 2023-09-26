ba7af1ce2d12abcd9fd9ff89f5c15c416787bbc3
iren57 <irenepeacemugai@gmail.com>
2023-09-26 08:03:17 +0300

    modified some codes

--- index.php
+++ index.php
@@ -1,11 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Learning Web Development</title>
+    <style>
+        img {
+            width: 100px;
+            border-radius: 50px;
+            float: left;
+            margin-right: 10px;
+        }
+
+        p {
+            font-weight: bold;
+        }
+    </style>
 </head>
+
 <body>
-    <h1>Welcome</h1>
+    <img src="img.png" alt="An Image about Html,Css and JavaScript" download>
+    <h1>Welcome.</h1>
+    <p>I Love using HTML,CSS AND JAVASCRIPT!</p>
 </body>
+
 </html>
